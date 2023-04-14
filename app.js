08ec52b9a3c1e5ddd1867cec6eb818f7e202b04b
Juan Bravo <bravojuan564@gmail.com>
2023-04-14 09:48:34 -0300

    "Fix reglas"

--- app.js
+++ app.js
@@ -4,7 +4,78 @@ const letraI= "imes";
 const letraO= "ober";
 const letraU= "ufat";
 const mediaQuery= matchMedia("(min-width: 320px)");
+const caracteresPermitidos = "^[a-z]+$"
 
+
+let formulario = document.getElementById("formulario");
+let textoCapturado= document.getElementById("texto")
+let btnEncriptar= document.getElementById("botonEncriptar")
+let btnDesencriptar= document.getElementById("botonDesencriptar")
+let btnCopiar = document.getElementById("botonCopiar")
+let textoAEncriptar = "";
+let textoADesencriptar = "";
+let textoAComprobar = ""
+
+
+const comprobarTexto = () =>{
+    
+    textoCapturado.addEventListener('input', () =>{
+    
+     textoAComprobar = textoCapturado.value
+    
+    if(textoAComprobar.match(caracteresPermitidos)){
+        
+        btnEncriptar.addEventListener('click',(e) =>{
+            textoAEncriptar= textoAComprobar
+    
+            if(textoAEncriptar.length !=0){
+                encriptarTexto()
+                eliminarElementos()
+                e.preventDefault()
+                btnCopiar.style.display = 'block';
+                return
+            }
+        })
+
+        btnDesencriptar.addEventListener('click',(e) =>{
+            textoADesencriptar= textoAComprobar
+
+            if (textoADesencriptar.length != 0) {
+               desencriptarTexto()
+               eliminarElementos()
+               e.preventDefault()
+               btnCopiar.style.display = 'block';
+               return
+            }
+        })
+    }else{
+        textoCapturado.value= ""
+        return
+        }
+    })
+}
+
+const encriptarTexto = () =>{
+   textoEncriptado = textoAEncriptar.replaceAll("e",letraE)
+                                    .replaceAll("i",letraI)
+                                    .replaceAll("a",letraA)
+                                    .replaceAll("o",letraO)
+                                    .replaceAll("u",letraU)
+    let resultado = document.getElementById("resultado-mensaje").textContent = textoEncriptado
+    formulario.reset()
+    return resultado
+}
+
+const desencriptarTexto = () =>{
+    textoDesencriptado = textoADesencriptar.replaceAll(letraE, "e")
+                                    .replaceAll(letraI, "i")
+                                    .replaceAll(letraA, "a")
+                                     .replaceAll(letraO, "o")
+                                     .replaceAll(letraU, "u")
+    let resultado = document.getElementById("resultado-mensaje").textContent =textoDesencriptado
+    formulario.reset()
+    return resultado
+} 
 
 const agregarElementos = mql =>{
     if(mql.matches){
@@ -21,39 +92,6 @@ const agregarElementos = mql =>{
     }
 }
 
-
-let formulario = document.getElementById("formulario");
-let textoCapturado= document.getElementById("texto");
-let btnEncriptar= document.getElementById("botonEncriptar")
-let btnDesencriptar= document.getElementById("botonDesencriptar")
-let btnCopiar = document.getElementById("botonCopiar")
-let textoAEncriptar = "";
-let textoADesencriptar = "";
-
-
-
-const encriptarTexto = () =>{
-   textoEncriptado = textoAEncriptar.replaceAll("e",letraE)
-                                    .replaceAll("i",letraI)
-                                    .replaceAll("a",letraA)
-                                    .replaceAll("o",letraO)
-                                    .replaceAll("u",letraU)
-    let resultado = document.getElementById("resultado-mensaje").textContent =textoEncriptado
-    formulario.reset()
-    return resultado
-}
-
-const desencriptarTexto = () =>{
-    textoDesencriptado = textoADesencriptar.replaceAll(letraE, "e")
-                                    .replaceAll(letraI, "i")
-                                    .replaceAll(letraA, "a")
-                                     .replaceAll(letraO, "o")
-                                     .replaceAll(letraU, "u")
-    let resultado = document.getElementById("resultado-mensaje").textContent =textoDesencriptado
-    formulario.reset()
-    return resultado
-} 
-
 const eliminarElementos = () =>{
     let etiquetaImg= document.querySelector("#mensajeEncriptado > img")
     let etiquetaP = document.querySelector("#mensajeEncriptado > p")
@@ -65,10 +103,10 @@ const eliminarElementos = () =>{
 const copiarTextoEncriptado = () => {
     let copiaEncriptado = encriptarTexto()
 
-    if(copiaEncriptado.length != 0){
+        if(copiaEncriptado.length != 0){
         navigator.clipboard.writeText(copiaEncriptado)
         document.getElementById("resultado-mensaje").textContent = ""
-        return
+        textoAEncriptar = ""
     }else{
         return
     }
@@ -76,32 +114,31 @@ const copiarTextoEncriptado = () => {
 
 const copiarTextoDensencriptado = () => {
     let copiaDensencriptado = desencriptarTexto()
-
-    if(copiaDensencriptado.length !=0){
+    
+    if(copiaDensencriptado.length != 0){
         navigator.clipboard.writeText(copiaDensencriptado)
         document.getElementById("resultado-mensaje").textContent = ""
+        textoADesencriptar = ""
         return
     }else{
         return
     }
 }
 
-btnEncriptar.addEventListener('click',(e) =>{
-    textoAEncriptar= textoCapturado.value.toLowerCase()
-    encriptarTexto()
-    eliminarElementos()
-    e.preventDefault()
-    btnCopiar.style.display = 'block';
-})
 
 btnDesencriptar.addEventListener('click',(e) =>{
      textoADesencriptar= textoCapturado.value.toLowerCase()
-     desencriptarTexto()
-     eliminarElementos()
-     e.preventDefault()
-     btnCopiar.style.display = 'block';
+     
+     if (textoADesencriptar.length != 0) {
+        desencriptarTexto()
+        eliminarElementos()
+        e.preventDefault()
+        btnCopiar.style.display = 'block';
+     }else{
+        return
+     }
+     
  })
-
 
 btnCopiar.addEventListener('click', () =>{
 
@@ -111,7 +148,7 @@ btnCopiar.addEventListener('click', () =>{
     btnCopiar.style.display = 'none';
 })
 
-
+comprobarTexto()
 
 
 
